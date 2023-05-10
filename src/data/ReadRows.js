176ab1a334350d8a3617f109234ad0b5888e1936
Thiebault Nicolas <nicolasthiebault80870@yahoo.com>
2023-05-10 16:40:05 +0200

    Connect DB

--- src/data/ReadRows.js
+++ src/data/ReadRows.js
@@ -1,12 +1,8 @@
 import { createClient } from '@supabase/supabase-js'
+import ConnectDB from './ConnectDB'
 
 function ReadRows () {
-
-const supabaseUrl = 'https://blfybbqpkivtglpsvida.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJsZnliYnFwa2l2dGdscHN2aWRhIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODM3MjY1MTQsImV4cCI6MTk5OTMwMjUxNH0.Xt9tGo7Dw789aAjUwQrPROsIGKbRYc__A3KSPB_uBvI'
-const supabase = createClient(supabaseUrl, supabaseKey)
-console.log(supabase);
-
+const SupaBase = ConnectDB()
 }
 
 export default ReadRows 
