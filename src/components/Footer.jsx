cc2032cb828544c01ac9140136049bb08ee920de
Thiebault Nicolas <nicolasthiebault80870@yahoo.com>
2023-05-20 16:53:21 +0200

    modif

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,29 +3,28 @@ import '../styles/Footer.css'
 
 function Footer({ isSalePage }) {
 
-     // Ajout de la classe conditionnellement
+  // Ajout de la classe conditionnellement
   const footerClass = isSalePage ? 'pieddepage footerSalePage' : 'pieddepage';
 
   return (
-
-     <footer className={footerClass}>
-                <div className="tableau"> 
-                    <h3>A propos</h3>     
-                    <a href="#">Fonctionnement du site </a>
-                    <a href="#">Conditions générales de vente </a>
-                    <a href="#">Données et confidentialité</a>
-                </div>
-                <div className="tableau" id="nos_hebergements">
-                    <h3> Nos meilleurs ventes</h3>       
-                    <a href="#">Charte qualité</a>
-                    <a href="#">Soumettre vos offres</a>
-                </div>
-                <div className="tableau" id="assistance">
-                    <h3>Assistance</h3>       
-                    <a href="#">Centre d'aide</a>
-                    <a href="#">Nous contacter</a>
-                </div>
-            </footer>  
+    <footer className={footerClass}>
+      <div className="tableau"> 
+        <h3>A propos</h3>     
+        <a href="#!" >Fonctionnement du site</a>
+        <a href="#!" >Conditions générales de vente</a>
+        <a href="#!" >Données et confidentialité</a>
+      </div>
+      <div className="tableau" id="nos_hebergements">
+        <h3>Nos meilleurs ventes</h3>       
+        <a href="#!" >Charte qualité</a>
+        <a href="#!" >Soumettre vos offres</a>
+      </div>
+      <div className="tableau" id="assistance">
+        <h3>Assistance</h3>       
+        <a href="#!" >Centre d'aide</a>
+        <a href="#!" >Nous contacter</a>
+      </div>
+    </footer>  
   )
 }
 
